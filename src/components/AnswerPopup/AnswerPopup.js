deb5965f6e61a79191723e5754f81ff6234b6064
TarStaBor <TarStaBor@yandex.ru>
2022-05-18 06:44:55 +0300

    fix: fix error on link

--- src/components/AnswerPopup/AnswerPopup.js
+++ src/components/AnswerPopup/AnswerPopup.js
@@ -9,7 +9,7 @@ function AnswerPopup(props) {
           <p className="answerPopup__title">{responseAnswer.title}</p>
           <p className="answerPopup__subtitle">{responseAnswer.subTitle}</p>
           {responseAnswer.link && (
-            <a href={`https://${responseAnswer.link}`} target="_blank" className="answerPopup__link">
+            <a href={`https://${responseAnswer.link}`} target="_blank" className="answerPopup__link" rel="noreferrer">
               {responseAnswer.link}
             </a>
           )}
